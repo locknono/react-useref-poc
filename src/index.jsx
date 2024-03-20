c57258ceb1837b6b73464730f7e119f467e7395f
react-app-scan <reactappscan@gmail.com>
2024-03-20 03:52:46 -0400

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,35 @@
 import React, { useState, useEffect, useCallback } from "react";
 import ReactDOM from "react-dom/client";
+import DOMPurify from "dompurify";
 
 import "./index.css";
 
-export default class Test extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo: "",
-    };
-  }
-  componentDidMount() {
-    this.setState({
-      foo: window.location.search,
-    });
-  }
-  render() {
-    return (
-      <>
-        <div dangerouslySetInnerHTML={{ __html: this.state.foo }} />
-      </>
-    );
-  }
+export default function Test(props) {
+  const content = window.location;
+
+  window.open(content);
+  document.write(window.location);
+  document.write(window.location.search);
+
+  document.getElementById("app").innerHTML = content;
+  window.location.replace(content);
+
+  const rootEl = document.getElementById("root");
+  const queryParams = new URLSearchParams(document.location.search);
+  const input = queryParams.get("input");
+
+  rootEl.innerHTML = input;
+
+  return (
+    <>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <a href={content} />
+      <iframe src={content} />
+    </>
+  );
 }
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Test />);
+
+const rootEl = document.getElementById("root");
+const queryParams = new URLSearchParams(document.location.search);
+const input = queryParams.get("input");
+rootEl.innerHTML = input; // Noncompliant
